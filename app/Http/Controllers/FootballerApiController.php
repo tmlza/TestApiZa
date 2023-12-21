0f0e82d6f5b95d087121e4a4e76c59775c935756
Tin Maung Linn <153699666+tmlza@users.noreply.github.com>
2023-12-21 11:54:47 +0630

    footballer api crud

--- app/Http/Controllers/FootballerApiController.php
+++ app/Http/Controllers/FootballerApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Footballer;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class FootballerApiController extends Controller
@@ -16,11 +17,41 @@ class FootballerApiController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage.    
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+ 
+        if ($validator->fails()) {
+            return response($validator->errors());
+        } else {
+            
+            $footballer = Footballer::create($request->all());
+
+            return response("$footballer",200);
+        
+        }
+       
+
+        // $validated = $request->validate([
+        //     'title' => 'required|max:255',
+        // ]);
+
+        // if(empty($validated)) {
+        //     $footballer = new Footballer;
+        //     $footballer->name = $request->name;   
+        //     $footballer->save();
+            
+        //     return response()->json($footballer,200);
+        // } else {
+        //     $data = [
+        //         "status"=>422
+        //     ];
+        //     return response()->json($data,422);
+        // }
     }
 
     /**
@@ -28,7 +59,7 @@ class FootballerApiController extends Controller
      */
     public function show(Footballer $footballer)
     {
-        //
+        return response("$footballer",200);
     }
 
     /**
@@ -36,7 +67,16 @@ class FootballerApiController extends Controller
      */
     public function update(Request $request, Footballer $footballer)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            "name"=> "required"
+            ]);
+
+        if ($validator->fails()) {
+            return response($validator->errors());
+        } else {
+            $footballer->update($request->all());
+            return response("$footballer",200);
+        }
     }
 
     /**
@@ -44,6 +84,7 @@ class FootballerApiController extends Controller
      */
     public function destroy(Footballer $footballer)
     {
-        //
+        $footballer->delete();
+        return response("Done!",200);
     }
 }
--- app/Models/Footballer.php
+++ app/Models/Footballer.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Footballer extends Model
 {
     use HasFactory;
+    protected $fillable = ["name"];
 }
